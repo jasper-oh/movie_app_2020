3ed7b51a77d8e375977a8027cf579e9268b24ec4
jasper <63331153+jasper-oh@users.noreply.github.com>
2020-12-28 17:36:54 +0900

    #2.1 setState, life cycle of component

--- src/App.js
+++ src/App.js
@@ -1,61 +1,26 @@
-import PropTypes from "prop-types"
+import React from "react"
+/*import PropTypes from "prop-types"*/
 
-function Syntax({name, director,image,rating}){
-  return(<div>
-    <h2>I like {name} made by {director}</h2>
-    <h4>rating {rating}/5 </h4>
-    <img src={image} alt="poster"/>
-  </div>);
-}
+class App extends React.Component{
+  state ={
+    count:0
+  }
+  add = () => {
+    this.setState(current => ({ count : current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count -1}));
+  };
 
-const movies = [
-  {
-    id:1,
-    title: "God Father",
-    director : "Francis Ford Coppola" ,
-    img :"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.VAUbK8bhXAxPqygIkoc-IwHaLF%26pid%3DApi&f=1",
-    rating : 5
-  },
-    
-  {
-    id:2,
-    title: "Harry Potter",
-    director:"Chris Columbus",
-    img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.iXaMxA6-L95IwJ_WujDoFAHaEK%26pid%3DApi&f=1",
-    rating : 4.5
-  },
-  {
-    id:3,
-    title: "Lord of the rings",
-    director:"Peter Jackson",
-    img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EBLTk-yVACjmdbB34nMnqQHaJn%26pid%3DApi&f=1",
-    rating :4.8
-  },
-  {
-  id:4,  
-  title: "heriditary",
-  director:"Ari Aster",
-  img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.IJgsgfgg3Yt2UdMg5gOnpgHaK-%26pid%3DApi&f=1",
-  rating : 5
-}
-];
 
-Syntax.propTypes ={
-  name:PropTypes.string.isRequired,
-  director:PropTypes.string.isRequired,
-  image:PropTypes.string.isRequired,
-  rating:PropTypes.number.isRequired
-};
+  render(){
+    return(<div>
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.add}>ADD</button>
+      <button onClick={this.minus}>MINUS</button>
+    </div>)
+  }
 
-function App() {
-  return (
-    <div className="App"> 
-    
-    {movies.map(movie => (<Syntax key={movie.id} name={movie.title} director={movie.director} image={movie.img} rating={movie.rating} /> ))}
-    
-    </div>
-    
-  );
 }
 
 export default App;
