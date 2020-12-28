6402da3857aba364d55e6f937acdf47aad61edf9
jasper <63331153+jasper-oh@users.noreply.github.com>
2020-12-28 15:28:50 +0900

    Dynamic Component Generate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+function Syntax({name, director,image}){
+  return(<div>
+    <h2>I like {name} made by {director}</h2>
+    <img src={image} alt="poster"/>
+  </div>);
+}
+
+const movies = [
+  {title: "God Father",
+    director : "Francis Ford Coppola" ,
+    img :"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.VAUbK8bhXAxPqygIkoc-IwHaLF%26pid%3DApi&f=1"
+  },
+    
+  {title: "Harry Potter",
+    director:"Chris Columbus",
+    img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.iXaMxA6-L95IwJ_WujDoFAHaEK%26pid%3DApi&f=1"
+  },
+  {title: "Lord of the rings",
+  director:"Peter Jackson",
+  img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EBLTk-yVACjmdbB34nMnqQHaJn%26pid%3DApi&f=1"
+},
+  {title: "heriditary",
+  director:"Ari Aster",
+  img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.IJgsgfgg3Yt2UdMg5gOnpgHaK-%26pid%3DApi&f=1"
+}
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    
+    {movies.map(movie => (<Syntax name={movie.title} director={movie.director} image={movie.img}/> ))}
+    
     </div>
+    
   );
 }
 
